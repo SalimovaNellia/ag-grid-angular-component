798be750d19d5a0b4592ad3b6f4c22eb58cc74c6
Salimova Nellia <salimova.nellia@gmail.com>
2021-07-20 13:08:46 +0300

    transformApiDataPipe

--- src/app/video-list/utils/tranform-api-data.pipe.ts
+++ src/app/video-list/utils/tranform-api-data.pipe.ts
@@ -1,15 +1,17 @@
-import {OperatorFunction} from "rxjs";
-import {ApiData, VideoItemFromApi, VideoListItem} from "../../shared /interfaces";
-import {map} from "rxjs/operators";
+import { OperatorFunction } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { ApiData, VideoItemFromApi, VideoListItem } from '../../shared /interfaces';
 
 export function transformApiDataToRowData(): OperatorFunction<ApiData, VideoListItem[]> {
-  return map<ApiData, VideoListItem[]>(({items}) => {
-    return items.map((item: VideoItemFromApi) => ({
-        thumbnail: item.snippet.thumbnails.default,
-        publishedOn: item.snippet.publishedAt,
-        videoTitle: {title: item.snippet.title, videoId: item.id.videoId},
-        description: item.snippet.description,
-      })
-    );
-  });
+  return map<ApiData, VideoListItem[]>(({items}) => (mapApiData(items)))
+}
+
+export function mapApiData(items: VideoItemFromApi[]) {
+  return items.map((item: VideoItemFromApi) => ({
+    thumbnail: item.snippet.thumbnails.default,
+    publishedOn: item.snippet.publishedAt,
+    videoTitle: {title: item.snippet.title, videoId: item.id.videoId},
+    description: item.snippet.description,
+  }))
 }
