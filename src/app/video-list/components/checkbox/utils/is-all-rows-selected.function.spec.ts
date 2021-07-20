e5768e169f61077ec2aa4372c372f782defaa1d1
Salimova Nellia <salimova.nellia@gmail.com>
2021-07-20 11:39:14 +0300

    test checkIsAllRowsSelected function

---
+++ src/app/video-list/components/checkbox/utils/is-all-rows-selected.function.spec.ts
@@ -0,0 +1,28 @@
+import {checkIsAllRowsSelected} from "./is-all-rows-selected.function";
+
+const mockResults = [true, false];
+const incomingValuesEquals = {
+  selectedRows: 10,
+  displayedRows: 10
+}
+
+const incomingValuesUnequals = {
+  selectedRows: 10,
+  displayedRows: 5
+}
+
+
+describe("checkIsAllRowsSelected", () => {
+  mockResults.forEach(mockResult => {
+    it(`should compare selected rows and return ${mockResult} if they are ${mockResult ? "equal" : "not equal"}`,
+      () => {
+      if (mockResult) {
+        const result = checkIsAllRowsSelected(incomingValuesEquals.selectedRows, incomingValuesEquals.displayedRows);
+        expect(result).toEqual(mockResult);
+      } else {
+        const result = checkIsAllRowsSelected(incomingValuesUnequals.selectedRows, incomingValuesUnequals.displayedRows);
+        expect(result).toEqual(mockResult);
+      }
+    })
+  })
+})
